dc192302cec37d7c328eb34ebbce33e52fab9bfc
EverettZh <67285078+EverettZh@users.noreply.github.com>
2022-11-20 09:05:10 +0000

    pretty much finished

--- trivia-generator/client/src/App.js
+++ trivia-generator/client/src/App.js
@@ -3,9 +3,27 @@ import { useState } from 'react';
 
 function App() {
   const API_URL = 'http://localhost:3001';
-
-  const [data, setData] = useState(null);
   const [prompt, setPrompt] = useState('');
+  const [school, setSchool] = useState('');
+  const [educationStatus, setEducationStatus] = useState('');
+  const {softSkills, setSoftSkills } = useState('');
+  const [data, setData] = useState('');
+  const {fullName, setFullName } = useState('');
+  const {email, setEmail } = useState('');
+  const {phoneNumber, setPhoneNumber } = useState('');
+  const { date, setDate } = useState('');
+  const { jobTitle, setJobTitle } = useState('');
+  const { company, setCompany } = useState('');
+  const { recruiterName, setRecruiterName } = useState('');
+  const { previousCompany1, setPreviousCompany1} = useState('');
+  const { activity1, setActivity1} = useState('');
+  const { aboutActivity1, setAboutActivity1} = useState('');
+  const { points1, setPoints1} = useState('');
+  const { previousCompany2, setpreviousCompany2} = useState('');
+  const { activity2, setActivity2} = useState('');
+  const { aboutActivity2, setAboutActivity2} = useState('');
+  const { points2, setPoints2} = useState('');
+  const { jobDescription, setJobDescription} = useState('');
 
   // Update prompt variable when handleChange is called
   const handleChange = (e, setChange) => {
@@ -14,6 +32,28 @@ function App() {
 
   // When handleSubmit is called, passes prompt to /api and then gets assigns response to data
   const handleSubmit = (e) => {
+    setPrompt(`Job Description: ${jobDescription}
+    School: ${school}
+    Education Status: ${educationStatus}
+    Soft Skills: ${softSkills}
+
+    Your Name: ${fullName}
+    Your Email: ${email}
+    Your Phone Number: ${phoneNumber}
+    Date: ${date}
+    Job Title: ${jobTitle}
+    Company: ${company}
+    Their Name: ${recruiterName}
+    Your Previous Group/Company: ${previousCompany1}
+    Your Previous Activity: ${activity1}
+    About Activity: ${aboutActivity1}
+    Points to Highlight: ${points1}
+    Your Previous Group/Company: ${previousCompany2}
+    Your Previous Activity: ${activity2}
+    About Activity: ${aboutActivity2}
+    Points to Highlight: ${points2}
+    
+    `);
     e.preventDefault();
     setData(null);
     fetch(`${API_URL}/api?prompt=${prompt}`)
@@ -26,15 +66,102 @@ function App() {
   <header className="App-header">
     <h1>Cover Letter AI</h1>
     <form onSubmit={handleSubmit}>
+    
+     <h3>Contact Information</h3>
+    <label>
+        Full Name:
+        <input name="full-name" rows="5" cols="5" value={fullName} onChange={(e) => handleChange(e, () => setFullName())} />
+      </label>
       <label>
-        Please select a prompt: <br />
-        <textarea name="input-box" rows="5" cols="100" value={prompt} onChange={handleChange(setPrompt)} />
+        Email:
+        <input name="email" rows="5" cols="5" value={email} onChange={(e) => handleChange(e, () => setEmail())} />
+        <br />
+      </label>
+      <label>
+        Phone Number:
+        <input name="phone-number" rows="5" cols="5" value={phoneNumber} onChange={(e) => handleChange(e, () => setPhoneNumber())} />
+      </label>
+      <label>
+        Date:
+        <input name="date" rows="5" cols="5" value={date} onChange={(e) => handleChange(e, () => setDate())} />
+        <br />
+      </label>
+      <label>
+        Job Title:
+        <input name="job-title" rows="5" cols="5" value={jobTitle} onChange={(e) => handleChange(e, () => setJobTitle())} />
+      </label>
+      <label>
+        Company:
+        <input name="company" rows="5" cols="5" value={company} onChange={(e) => handleChange(e, () => setCompany())} />
+        <br />
+      </label>
+      <label>
+        Recruiter Name:
+        <input name="recruiter-name" rows="5" cols="5" value={recruiterName} onChange={(e) => handleChange(e, () => setRecruiterName())} />
+      </label>
+      <h3>Education and Soft Skills</h3>
+    <label>
+        School:
+        <input name="school" rows="5" cols="5" value={school} onChange={(e) => handleChange(e, () => setSchool())} />
+      </label>
+      <label>
+        Education Status:
+        <input name="email" rows="5" cols="5" value={educationStatus} onChange={(e) => handleChange(e, () => setEducationStatus())} />
+        <br />
+      </label>
+      <label>
+        Soft Skills:
+        <input name="soft-skills" rows="5" cols="5" value={softSkills} onChange={(e) => handleChange(e, () => setSoftSkills())} />
+      </label>
+      <h3>Experience 1</h3>
+      <label>
+        Previous Company:
+        <input name="previous-company1" rows="5" cols="5" value={previousCompany1} onChange={(e) => handleChange(e, () => setPreviousCompany1())} />
+      </label>
+      <label>
+        Activity:
+        <input name="activity1" rows="5" cols="5" value={activity1} onChange={(e) => handleChange(e, () => setActivity1())} />
+        <br />
+      </label>
+      <label>
+        About Activity:
+        <input name="about-activity1" rows="5" cols="5" value={aboutActivity1} onChange={(e) => handleChange(e, () => setAboutActivity1())} />
+      </label>
+      <label>
+        Points to Emphasize:
+        <input name="points-to-emphasize1" rows="5" cols="5" value={points1} onChange={(e) => handleChange(e, () => setPoints1())} />
+        <br />
+      </label>
+      <h3>Experience 2</h3>
+      <label>
+        Previous Company:
+        <input name="previous-company2" rows="5" cols="5" value={previousCompany2} onChange={(e) => handleChange(e, () => setpreviousCompany2())} />
+      </label>
+      <label>
+        Activity:
+        <input name="activity2" rows="5" cols="5" value={activity2} onChange={(e) => handleChange(e, () => setActivity2())} />
+        <br />
+      </label>
+      <label>
+        About Activity:
+        <input name="about-activity2" rows="5" cols="5" value={aboutActivity2} onChange={(e) => handleChange(e, () => setAboutActivity2())} />
+      </label>
+      <label>
+        Points to Emphasize:
+        <input name="points-to-emphasize2" rows="5" cols="5" value={points2} onChange={(e) => handleChange(e, () => setPoints2())} />
+        <br />
+      </label>
+      <br />
+      <label>
+        Enter Job Description Here: 
+        <br />
+        <textarea name="input-box" rows="5" cols="100" value={jobDescription} onChange={(e) => handleChange(e, () => setJobDescription())} />
       </label>
       <br />
       <input type="submit" value="Submit" />
     </form>
     <h1>Result:</h1>
-    <h3>{!data ? 'Question will appear here.' : data}</h3>
+    <h4>{!data ? 'Cover letter will appear here.' : data}</h4>
   </header>
 </div>
 )
--- trivia-generator/server/index.js
+++ trivia-generator/server/index.js
@@ -1,7 +1,71 @@
 // server/index.js
 
-import { AIData } from '../client/src/data';
+const AIData = `This program will generate a cover letter based on the user data being given:
 
+--
+Job Description: 
+
+
+Apple is a place where extraordinary people gather to do their best work. Together we create products and experiences people once couldn’t have envisioned — and now can’t imagine living without. If you’re excited by the idea of making an impact, joining a team where we pride ourselves in being one of the most diverse and expansive companies in the world, a career with Apple might be your dream job. 
+
+Quite simply, our teams keep Apple’s world-class networks running. We are a global community of engineers who work together to operate a multi-vendor network that interconnects Apple’s large data centres, campuses and smaller regional or national offices, as well as providing connectivity via various VPN technologies to third-party suppliers and partners. This is all glued together across an Internet-scale backbone network. 
+
+We are looking for an enthusiastic and passionate person to join a dynamic and exciting team within Apple’s Global Infrastructure Operations, supporting Apple’s varied networks. You will be skilled in cross-organisational collaboration, liaising with internal teams and external service providers to operate and maintain the network infrastructure. You will need to develop and maintain clear documentation and procedures associated with operations and known issues. This position provides the opportunity to operate a cutting-edge, worldwide, multi-vendor network.
+Key Qualifications
+Working experience with IPv4, IPv6, BGP, OSPF and layer 2 technologies.
+Good understanding of large-scale LAN and WAN networking technologies.
+Knowledge of VPN technologies such as IPSec and DMVPN.
+Experience with Palo Alto, Juniper, Cisco or other vendors’ firewalls.
+Ability to coordinate and troubleshoot with internal and external partner teams.
+Proficiency with Linux or other versions of UNIX.
+Knowledge of configuration management and automation technologies such as Ansible, Salt, Chef and Python, Go, etc.
+Description
+
+School: CalTech
+Education Status:
+Soft Skills: 
+
+Your Name: Wilma Dean
+Your Email: WilmaDean@hotmail.com
+Your Phone Number: 6723291110
+Date: December 12, 2018
+Job Title: Network Engineer
+Company: Apple
+Their Name: Johan Mantell
+Your Previous Group/Company: CalTech
+Your Previous Activity: Internal work experience at Tokyo Tech Conferance
+About Activity: Cisco certifications 
+Points to highlight: Continuous improvement
+Your Previous Group/Company: Sansong
+Your Previous Activity: Network Optimization
+About Activity: Reduced security issues
+Points to highlight: Reduced by 35% over two years, encryption protocols received recognition.
+
+Cover Letter Sample:
+
+Dear Mr. Mantell,
+
+Having worked as a network engineer at Sansong for the past five years, I understand the varied requirements of the role at Apple. My previous experience in infrastructure design will also assist in the considerable project-led aspects of the position.
+
+My computer science degree from CalTech was followed up with a year of international work experience at the Tokyo Tech Conference. I embarked on my Cisco certifications soon after beginning my first role and I have been on a journey of continuous improvement since.
+
+I have been a keen participant in security hackathons over the past five years and it was my team mate Sarah Jenwith who alerted me to the role at Apple. We came second in a recent State competition, and she told me much about the challenges that you are currently facing. My network optimization work at Sansong reduced security issues by 35% over a two-year period and my encryption protocols received company-wide recognition. In terms of my technical expertise, I have experience in the following:
+
+LAN, VPN, SAN and VoIP infrastructure design
+Network and security admin, design, analysis and improvement
+Cloud computing systems with encrypted data storage facilities
+ 
+My network support decreased downtime for my department by 7% and we improved malware detection by 45%. This resulted in an estimated business saving of $2.5m and is something that I know I can replicate at Apple.
+
+I would welcome the opportunity of an interview to find out more about the challenges and specifics of the role.
+
+Sincerely,
+Wilma Dean
+
+--
+`;
+
+const coverLetterSample = "Cover Letter Sample:/n";
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
@@ -19,10 +83,10 @@ cohere.init('UdLGZiYuylCQRxdqP8OcPM8MtoFBn9wlnW1BfNC6');
 app.get('/api', async (req, res) => {
   const response = await cohere.generate({
     model: 'large',
-    prompt: AIData,
-    max_tokens: 50,
-    temperature: 0.6,
-    stop_sequences: ['-'],
+    prompt: req + coverLetterSample,
+    max_tokens: 320,
+    temperature: 2,
+    stop_sequences: ['--'],
   });
   res.json(response.body);
 });
